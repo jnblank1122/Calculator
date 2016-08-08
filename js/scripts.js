b89edac9e49132e59c2bd3441171bd320ca235a8
Anand Angalig and Jake Blank <jake@>
2016-08-08 10:47:58 -0400

    Add first js to webpage.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var num1 = parseInt(prompt("Enter a number: "));
+var num2 = parseInt(prompt("Enter another number: "));
+var add = function(num1,num2) {return(num1+num2);};
+
+alert(add(num1,num2));
