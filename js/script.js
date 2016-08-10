68e81d316de6c4025461a49aec4575302cbd479b
Alexandre Leibler and Jake Blank <alexandre@>
2016-08-10 10:53:24 -0400

    Update js function for add sub mult and div.

--- js/script.js
+++ js/script.js
@@ -11,53 +11,36 @@ var sub = function(num1, num2){
   return num1 - num2;
 };
 
+var currentfunction;
 
 $("document").ready(function(){
 
-  var getInput = $("form#input").submit(function(event) {
+  $("form#input").submit(function(event) {
     event.preventDefault();
-    var num1 = parseInt($("#num1").val());
-    var num2 = parseInt($("#num2").val());
-
-    var numArray = [num1, num2];
-    return numArray;
+    var first = parseInt($("#num1").val());
+    var second = parseInt($("#num2").val());
+    if (currentfunction ==="add"){
+      var answer = add(first, second);
+      $("#result").text(answer);
+    }
+    else if (currentfunction ==="mult"){
+      var answer = mult(first, second);
+      $("#result").text(answer);
+    }
+    else if (currentfunction ==="div"){
+      var answer = div(first, second);
+      $("#result").text(answer);
+    }
+    else if (currentfunction ==="sub"){
+      var answer = sub(first, second);
+      $("#result").text(answer);
+    }
+    //currentfunction(first,second);
   });
 
-
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    debugger;
-    var num1 = parseInt(getInput().charAt(0));
-    var num2 = parseInt(getInput().charAt(1));
-    var result = add(num1, num2);
-    $("#result").text(result);
-  });
-
-  $("form#mult").submit(function(event) {
-    event.preventDefault();
-    debugger;
-    var num1 = parseInt(getInput().charAt(0));
-    var num2 = parseInt(getInput().charAt(1));
-    var result = mult(num1, num2);
-    $("#result").text(result);
-  });
-
-  $("form#div").submit(function(event) {
-    event.preventDefault();
-    debugger;
-    var num1 = parseInt(getInput().charAt(0));
-    var num2 = parseInt(getInput().charAt(1));
-    var result = div(num1, num2);
-    $("#result").text(result);
-  });
-
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    debugger;
-    var num1 = parseInt(getInput().charAt(0));
-    var num2 = parseInt(getInput().charAt(1));
-    var result = sub(num1, num2);
-    $("#result").text(result);
+  $(".btn").click(function(event){
+    currentfunction=$(this).attr("id");
+    console.log(currentfunction);
   });
 
 });
