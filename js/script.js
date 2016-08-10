64fbe44ec89d2bc8f98e7309b1143ac17a02c9a8
Alexandre Leibler and Jake Blank <alexandre@>
2016-08-10 11:42:24 -0400

    refactor code to get rid of global var

--- js/script.js
+++ js/script.js
@@ -11,36 +11,29 @@ var sub = function(num1, num2){
   return num1 - num2;
 };
 
-var currentfunction;
-
 $("document").ready(function(){
 
-  $("form#input").submit(function(event) {
+  $("form#input").submit(function(event){
     event.preventDefault();
-    var first = parseInt($("#num1").val());
-    var second = parseInt($("#num2").val());
-    if (currentfunction ==="add"){
-      var answer = add(first, second);
-      $("#result").text(answer);
-    }
-    else if (currentfunction ==="mult"){
-      var answer = mult(first, second);
-      $("#result").text(answer);
-    }
-    else if (currentfunction ==="div"){
-      var answer = div(first, second);
-      $("#result").text(answer);
-    }
-    else if (currentfunction ==="sub"){
-      var answer = sub(first, second);
-      $("#result").text(answer);
-    }
-    //currentfunction(first,second);
   });
 
   $(".btn").click(function(event){
-    currentfunction=$(this).attr("id");
-    console.log(currentfunction);
+    var currentFunction=$(this).attr("id");
+    var first = parseInt($("#num1").val());
+    var second = parseInt($("#num2").val());
+    if (currentFunction ==="add"){
+      var answer = add(first, second);
+    }
+    else if (currentFunction ==="mult"){
+      var answer = mult(first, second);
+    }
+    else if (currentFunction ==="div"){
+      var answer = div(first, second);
+    }
+    else {
+      var answer = sub(first, second);
+    }
+    $("#result").text(answer);
   });
 
 });
